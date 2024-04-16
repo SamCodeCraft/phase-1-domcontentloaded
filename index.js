08b30591f8ed0f92205971ca321a86609f0d5c75
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-04-16 12:59:07 +0300

    DOMContedLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    let paragraph = document.getElementById('text');
+    
+    // Replace the text with "This is really cool!"
+    paragraph.textContent = "This is really cool!";
+});
